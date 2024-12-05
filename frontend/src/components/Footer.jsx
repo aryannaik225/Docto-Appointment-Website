702a2d2645fd61670781030e2ba0c421ea16a150
Aryan Naik <aryann2203@gmail.com>
2024-12-05 22:33:38 +0530

    just saving

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import { assets } from "../assets/assets_frontend/assets"
+
+
+const Footer = () => {
+  return (
+    <div>
+      <div>
+
+        {/* -------- Left Section -------- */}
+        <div>
+          <img src={assets.logo} alt="" />
+        </div>
+
+        {/* -------- Middle Section -------- */}
+        <div>
+          
+        </div>
+
+        {/* -------- Right Section -------- */}
+        <div>
+          
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer
