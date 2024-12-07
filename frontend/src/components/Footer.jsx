515ffe05d8970117d807413f65f0da12efa3154d
Aryan Naik <aryann2203@gmail.com>
2024-12-07 20:50:28 +0530

    footer done, homepage done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,24 +3,43 @@ import { assets } from "../assets/assets_frontend/assets"
 
 const Footer = () => {
   return (
-    <div>
-      <div>
+    <div className="md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
 
         {/* -------- Left Section -------- */}
         <div>
-          <img src={assets.logo} alt="" />
+          <img src={assets.logo} alt="" className="mb-5 w-40"/>
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor autem minima, inventore cupiditate velit optio nostrum fuga sequi quae aliquam facere. Placeat a earum error assumenda ipsam exercitationem cumque esse.
+          </p>
         </div>
 
         {/* -------- Middle Section -------- */}
         <div>
-          
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Privacy policy</li>
+          </ul>
         </div>
 
         {/* -------- Right Section -------- */}
         <div>
-          
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>+1-212-456-7890</li>
+            <li>randomemail@gmail.com</li>
+          </ul>
         </div>
 
+      </div>
+
+      {/* -------- Copyright text -------- */}
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">Copyright 2024@ AryanNaik - All Rights Reserved.</p>
       </div>
     </div>
   )
