0c73a13123708ae96cbd2f3b30cff927ad7c4f55
aryannaik225 <aryann2203@gmail.com>
2024-12-12 23:03:40 +0530

    just saving

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -46,6 +46,14 @@ const Navbar = () => {
                 </div>
                 : <button onClick={() => navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light md:block hidden'>Create Account</button>
             }
+            <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden ' src={assets.menu_icon} alt="" />
+            {/* --- Mobile Menu --- */}
+            <div>
+                <div>
+                    <img src={assets.logo} alt="" />
+                    <img onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
+                </div>
+            </div>
         </div>
     </div>
   )
